923f2f74e64b78b0532e5e69bd54f9e25615dcd3
Jhoan_git1 <jhoanlucky6370@gmail.com>
2025-08-25 18:19:58 -0500

    Delete Proyecto_MesaAyuda directory

--- Proyecto_MesaAyuda/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Conexión a la base de datos MySQL
-$conexion = new mysqli("localhost", "root", "", "mesa_ayuda");
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-?>
